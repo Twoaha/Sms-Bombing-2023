c2ca956e3804b6e190a63db29102877658851be1
Twoaha Ibn Jalal <108892202+Twoaha@users.noreply.github.com>
2023-01-09 12:40:31 +0600

    Twoaha.py

---
+++ Twoaha.py
@@ -0,0 +1,27 @@
+#<\>!python3.11
+
+#-------------------Dont Edit This Script-----------------#
+
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="64bit":
+
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+
+    time.sleep(2)
+
+    import MX.mahadi
+
+else:
+
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+
+#---------------------------------------------------------#
+
+#                    THIS TOOL OWNED BY
+
+#                  
+
+#---------------------------------------------------------#
